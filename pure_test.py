99c9443bb00c2fccc7aa03bc37ada7f0b1ab97db
Filippo Vicentini <filippovicentini@gmail.com>
2020-11-09 17:29:20 +0100

    add test

--- pure_test.py
+++ pure_test.py
@@ -3,6 +3,7 @@ import jax
 from jax.experimental import stax
 import numpy as np
 from netket.machine import jax as nkj
+from netket._dynamics import TimeEvolution
 
 L = 4
 g = nk.graph.Grid([L])
@@ -44,6 +45,14 @@ lloc = nk.operator.local_values(lind, ma, vv)
 
 op = nk.optimizer.Sgd(ma, learning_rate=0.001)
 
-gs = nk.SteadyStatePure(lind, sa, op, n_samples=1000, sr=None, n_discard=100)
+sr = nk.optimizer.SR(ma, diag_shift=0.01)
 
-gs.run(1)
+# gs = nk.SteadyStatePure(lind, sa, op, n_samples=1000, sr=sr, n_discard=100)
+
+# gs.run(1)
+# gs.run(100)
+
+tevo = TimeEvolution(lind, sa, sr=sr, n_samples=1000, n_discard=100)
+tevo.solver("euler", (0.0, 20.0), dt=0.001, adaptive=False)
+
+tevo.run(20.0, out="test2", step_size=1e-10)
