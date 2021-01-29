70e34445b0a53f4ab5177cf0914eeaadb55308b7
yannra <yannic.rath@kcl.ac.uk>
2021-01-29 13:19:13 +0000

    Adjust parameters

--- scripts/qgps_2d_100_j1_j2_complex.py
+++ scripts/qgps_2d_100_j1_j2_complex.py
@@ -63,13 +63,13 @@ ma = nk.machine.QGPSSumSym(hi, n_bond=N, automorphisms=transl, spin_flip_sym=Tru
 ma.init_random_parameters(sigma=0.1)
 
 # Optimizer
-op = nk.optimizer.Sgd(ma, learning_rate=0.02)
+op = nk.optimizer.Sgd(ma, learning_rate=0.01)
 
 # Sampler
 sa = nk.sampler.MetropolisExchange(machine=ma,graph=g,d_max=2, n_chains=1)
 
 # Stochastic Reconfiguration
-sr = nk.optimizer.SR(ma, diag_shift=0.02)
+sr = nk.optimizer.SR(ma, diag_shift=0.05)
 
 samples = 20000
 
--- scripts/qgps_2d_36_heisenberg_prod_sym.py
+++ scripts/qgps_2d_36_heisenberg_prod_sym.py
@@ -50,13 +50,13 @@ ma = nk.machine.QGPSProdSym(hi, n_bond=N, automorphisms=transl, spin_flip_sym=Tr
 ma.init_random_parameters(sigma=0.1)
 
 # Optimizer
-op = nk.optimizer.Sgd(ma, learning_rate=0.04)
+op = nk.optimizer.Sgd(ma, learning_rate=0.02)
 
 # Sampler
 sa = nk.sampler.MetropolisExchange(machine=ma,graph=g,d_max=1, n_chains=1)
 
 # Stochastic Reconfiguration
-sr = nk.optimizer.SR(ma, diag_shift=0.02)
+sr = nk.optimizer.SR(ma, diag_shift=0.03)
 
 samples = 10000
 
