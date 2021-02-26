8c6d79500f61da7dff32246f765841b70d5d870c
yannra <yannic.rath@kcl.ac.uk>
2021-02-26 18:34:54 +0000

    Modify no MSR 100 sites script

--- scripts/qgps_2d_100_j1_j2_complex_no_msr.py
+++ scripts/qgps_2d_100_j1_j2_complex_no_msr.py
@@ -62,7 +62,7 @@ for mat, site in zip(mats, sites):
 transl = symmetries.get_symms_square_lattice(L)
 
 ma = nk.machine.QGPSSumSym(hi, n_bond=N, automorphisms=transl, spin_flip_sym=True, dtype=complex)
-ma.init_random_parameters(start_from_uniform=False)
+ma.init_random_parameters(sigma=0.05, start_from_uniform=False)
 
 # Optimizer
 op = nk.optimizer.Sgd(ma, learning_rate=0.01)
