765e8ddec8e3444166f142c968365c248e03e0c7
yannra <yannic.rath@kcl.ac.uk>
2021-02-26 19:14:42 +0000

    Adjust 100 sites no MSR script

--- scripts/qgps_2d_100_j1_j2_complex_no_msr.py
+++ scripts/qgps_2d_100_j1_j2_complex_no_msr.py
@@ -62,17 +62,17 @@ for mat, site in zip(mats, sites):
 transl = symmetries.get_symms_square_lattice(L)
 
 ma = nk.machine.QGPSSumSym(hi, n_bond=N, automorphisms=transl, spin_flip_sym=True, dtype=complex)
-ma.init_random_parameters(sigma=0.05, start_from_uniform=False)
+ma.init_random_parameters(start_from_uniform=False)
 
 # Optimizer
-op = nk.optimizer.Sgd(ma, learning_rate=0.01)
+op = nk.optimizer.Sgd(ma, learning_rate=0.02)
 
 # Sampler
 sa = nk.sampler.MetropolisExchange(machine=ma,graph=g,d_max=2, n_chains=1)
 sa.reset(True)
 
 # Stochastic Reconfiguration
-sr = nk.optimizer.SR(ma, lsq_solver="cg")
+sr = nk.optimizer.SR(ma)
 
 arr = np.zeros(ma._epsilon.size, dtype=bool)
 arr[:opt_par] = True
