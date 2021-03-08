f0033461f81a2572b9df18487650745a7e31780f
yannra <yannic.rath@kcl.ac.uk>
2021-03-08 16:44:40 +0000

    Modify 1D Heisenberg script

--- scripts/qgps_1d_heisenberg.py
+++ scripts/qgps_1d_heisenberg.py
@@ -39,14 +39,42 @@ op = nk.optimizer.Sgd(ma, learning_rate=0.02)
 
 # Sampler
 sa = nk.sampler.MetropolisExchange(machine=ma,graph=g,d_max=1,n_chains=1)
+sa.reset(True)
 
 # Stochastic Reconfiguration
 sr = nk.optimizer.SR(ma)
 
 samples = max(5000, ma._epsilon.size * 5)
 
+max_opt = 2500
+
+arr = np.zeros(ma._epsilon.size, dtype=bool)
+arr[:max_opt] = True
+max_id = min(max_opt, arr.size)
+
+class SiteSweepOpt(nk.Vmc):
+    def iter(self, n_steps, step=1):
+        global max_id
+        count = 0
+        for _ in range(0, n_steps, step):
+            for i in range(0, step):
+                ma.change_opt_ids(arr.reshape(ma._epsilon.shape))
+                dp = self._forward_and_backward()
+                self.update_parameters(dp)
+                arr.fill(False)
+                arr[max_id:(max_id+max_opt)] = True
+                if max_id + max_opt > arr.size:
+                    arr[:(max_id + max_opt - arr.size)] = True
+                    max_id = min((max_id + max_opt - arr.size), arr.size)
+                else:
+                    max_id = min((max_id + max_opt), arr.size)
+                if i == 0:
+                    yield self.step_count
+                count += 1
+
+
 # Create the optimization driver
-gs = nk.Vmc(hamiltonian=ha, sampler=sa, optimizer=op, n_samples=samples, sr=sr, n_discard=100)
+gs = SiteSweepOpt(hamiltonian=ha, sampler=sa, optimizer=op, n_samples=samples, sr=sr)
 
 if mpi.COMM_WORLD.Get_rank() == 0:
     with open("out.txt", "w") as fl:
