0c7b1fef1fc3fa01c8965f276a5014df05e70627
yannra <yannic.rath@kcl.ac.uk>
2021-03-09 21:33:16 +0000

    Add 1D lin model script

---
+++ scripts/qgps_1d_heisenberg_lin_model.py
@@ -0,0 +1,73 @@
+import numpy as np
+import netket as nk
+import sys
+import mpi4py.MPI as mpi
+import symmetries
+
+L = int(sys.argv[1])
+N = int(sys.argv[2])
+mode = int(sys.argv[3])
+
+rank = mpi.COMM_WORLD.Get_rank()
+
+if rank == 0:
+    with open("result.txt", "w") as fl:
+        fl.write("L, N, energy (real), energy (imag), energy_error\n")
+
+g = nk.graph.Hypercube(length=L, n_dim=1, pbc=True)
+
+# Spin based Hilbert Space
+hi = nk.hilbert.Spin(s=0.5, total_sz=0.0, N=g.n_nodes)
+
+ha = nk.operator.Heisenberg(hi, g, J=0.25)
+
+transl = symmetries.get_symms_chain(L)
+
+ma = nk.machine.QGPSLinExp(hi, n_bond_lin=N, n_bond_exp=0, automorphisms=transl, spin_flip_sym=True, dtype=float)
+
+ma.init_random_parameters(sigma=0.1)
+
+# Optimizer
+op = nk.optimizer.Sgd(ma, learning_rate=0.02)
+
+# Sampler
+sa = nk.sampler.MetropolisExchange(machine=ma,graph=g,d_max=1,n_chains=1)
+sa.reset(True)
+
+# Stochastic Reconfiguration
+sr = nk.optimizer.SR(ma)
+
+samples = L * 100
+
+# Create the optimization driver
+gs = nk.Vmc(hamiltonian=ha, sampler=sa, optimizer=op, n_samples=samples, sr=sr)
+
+if mpi.COMM_WORLD.Get_rank() == 0:
+    with open("out.txt", "w") as fl:
+        fl.write("")
+
+for it in gs.iter(1950,1):
+    if mpi.COMM_WORLD.Get_rank() == 0:
+        print(it,gs.energy)
+        with open("out.txt", "a") as fl:
+            fl.write("{}  {}  {}\n".format(np.real(gs.energy.mean), np.imag(gs.energy.mean), gs.energy.error_of_mean))
+
+epsilon_avg = np.zeros(ma._epsilon_lin.shape, dtype=ma._epsilon.dtype)
+
+for it in gs.iter(50,1):
+    epsilon_avg += ma._epsilon_lin
+    if mpi.COMM_WORLD.Get_rank() == 0:
+        print(it,gs.energy)
+        with open("out.txt", "a") as fl:
+            fl.write("{}  {}  {}\n".format(np.real(gs.energy.mean), np.imag(gs.energy.mean), gs.energy.error_of_mean))
+
+epsilon_avg /= 50
+
+ma._epsilon_lin = epsilon_avg
+
+est = nk.variational.estimate_expectations(ha, sa, 50000, n_discard=100)
+
+if mpi.COMM_WORLD.Get_rank() == 0:
+    np.save("epsilon_avg.npy", ma._epsilon_lin)
+    with open("result.txt", "a") as fl:
+        fl.write("{}  {}  {}  {}  {}\n".format(L, N, np.real(est.mean), np.imag(est.mean), est.error_of_mean))
