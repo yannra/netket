865b2ad4cb6eb58294d70196e60c8019f72fd2ee
yannra <yannic.rath@kcl.ac.uk>
2021-03-28 15:04:14 +0100

    Add linear method SweepOpt, sweep by bonds default

--- netket/custom/__init__.py
+++ netket/custom/__init__.py
@@ -1,5 +1,5 @@
 from .hamiltonians import J1J2
 from .symmetries import get_symms_chain, get_symms_square_lattice
-from .sweep_opt import SweepOpt
 from ._der_local_values import local_values_with_der
 from .linear_method import LinMethod
+from .sweep_opt import SweepOpt, SweepOptLinMethod
--- netket/custom/sweep_opt.py
+++ netket/custom/sweep_opt.py
@@ -1,5 +1,6 @@
 import netket as nk
 import numpy as np
+from .linear_method import LinMethod
 
 class SweepOpt(nk.Vmc):
     def __init__(
@@ -11,7 +12,7 @@ class SweepOpt(nk.Vmc):
         n_discard=None,
         sr=None,
         max_opt = 3000,
-        sweep_by_bonds = False,
+        sweep_by_bonds = True,
     ):
         super().__init__(hamiltonian, sampler, optimizer, n_samples, n_discard=n_discard, sr=sr)
         self.max_opt = max_opt
@@ -38,6 +39,58 @@ class SweepOpt(nk.Vmc):
                 self._sampler._machine.change_opt_ids(opt_tensor)
                 if self.sr is not None and self.max_opt < self.opt_arr.size:
                     self.optimizer.sr._x0 = None
+                dp = self._forward_and_backward()
+                self.update_parameters(dp)
+                self.opt_arr.fill(False)
+                self.opt_arr[self.max_id:(self.max_id+self.max_opt)] = True
+                if self.max_id + self.max_opt > self.opt_arr.size:
+                    self.opt_arr[:(self.max_id + self.max_opt - self.opt_arr.size)] = True
+                    self.max_id = min((self.max_id + self.max_opt - self.opt_arr.size), self.opt_arr.size)
+                else:
+                    self.max_id = min((self.max_id + self.max_opt), self.opt_arr.size)
+                if i == 0:
+                    yield self.step_count
+
+
+class SweepOptLinMethod(LinMethod):
+    def __init__(
+        self,
+        hamiltonian,
+        sampler,
+        optimizer,
+        n_samples,
+        shift = 1,
+        epsilon = 0.5,
+        update_shift = True,
+        corr_samp = None,
+        n_discard=None,
+        max_opt = 3000,
+        sweep_by_bonds = True,
+    ):
+        super().__init__(hamiltonian, sampler, optimizer, n_samples, shift=shift, epsilon=epsilon,
+                         update_shift=update_shift, corr_samp=corr_samp, n_discard=n_discard)
+        self.max_opt = max_opt
+
+        self.opt_arr = np.zeros(self._sampler._machine._epsilon.size, dtype=bool)
+        self.opt_arr[:self.max_opt] = True
+        self.max_id = min(self.max_opt, self.opt_arr.size)
+        self.sweep_by_bonds = sweep_by_bonds
+
+    def iter(self, n_steps, step=1):
+        for _ in range(0, n_steps, step):
+            for i in range(0, step):
+                if self.sweep_by_bonds:
+                    opt_tensor = np.zeros(self._sampler._machine._epsilon.shape, dtype=bool)
+                    arr_count = 0
+                    for k in range(self._sampler._machine._epsilon.shape[1]):
+                        for j in range(self._sampler._machine._epsilon.shape[0]):
+                            for l in range(self._sampler._machine._epsilon.shape[2]):
+                                opt_tensor[j,k,l] = self.opt_arr[arr_count]
+                                arr_count += 1
+                else:
+                    opt_tensor = self.opt_arr.reshape(self._sampler._machine._epsilon.shape)
+
+                self._sampler._machine.change_opt_ids(opt_tensor)
                 dp = self._forward_and_backward()
                 self.update_parameters(dp)
                 self.opt_arr.fill(False)
