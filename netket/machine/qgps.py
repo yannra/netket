184ddbe8a224c720899ed4117ad4189ad62b2225
yannra <yannic.rath@kcl.ac.uk>
2021-04-09 11:08:58 +0100

    QGPS change init linexp

--- netket/machine/qgps.py
+++ netket/machine/qgps.py
@@ -766,8 +766,8 @@ class QGPSLinExp(AbstractMachine):
         return self._npar
     
     def init_random_parameters(self, seed=None, sigma=0.1):
-        epsilon_lin = _np.zeros(self._epsilon_lin.shape, dtype=self._npdtype)
-        epsilon_exp = _np.zeros(self._epsilon_exp.shape, dtype=self._npdtype)
+        epsilon_lin = _np.ones(self._epsilon_lin.shape, dtype=self._npdtype)
+        epsilon_exp = _np.ones(self._epsilon_exp.shape, dtype=self._npdtype)
 
         if _rank == 0:
             rgen = _np.random.default_rng(seed)
@@ -776,12 +776,6 @@ class QGPSLinExp(AbstractMachine):
             if self._dtype == complex:
                 epsilon_lin += 1j*rgen.normal(scale=sigma, size=epsilon_lin.shape)
                 epsilon_exp += 1j*rgen.normal(scale=sigma, size=epsilon_exp.shape)
-            for i in range(epsilon_lin.shape[0]):
-                for j in range(epsilon_lin.shape[1]):
-                    epsilon_lin[i, j, :] /= _np.max(abs(epsilon_lin[i, j, :]))
-            for i in range(epsilon_exp.shape[0]):
-                for j in range(epsilon_exp.shape[1]):
-                    epsilon_exp[i, j, :] /= _np.max(abs(epsilon_exp[i, j, :]))
 
         if _n_nodes > 1:
             _MPI_comm.Bcast(epsilon_lin, root=0)
