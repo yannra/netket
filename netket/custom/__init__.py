8a2b000e2f45f1804668953bf9c9492e801a201c
yannra <yannic.rath@kcl.ac.uk>
2021-04-13 18:49:14 +0100

    Add stabilised SR optimiser

--- netket/custom/__init__.py
+++ netket/custom/__init__.py
@@ -2,4 +2,5 @@ from .hamiltonians import J1J2
 from .symmetries import get_symms_chain, get_symms_square_lattice
 from ._der_local_values import local_values_with_der
 from .linear_method import LinMethod
+from .stabilised_sr import SRStab
 from .sweep_opt import SweepOpt, SweepOptLinMethod
---
+++ netket/custom/stabilised_sr.py
@@ -0,0 +1,178 @@
+import math
+
+import netket as _nk
+
+from netket.operator import local_values as _local_values
+from netket.custom import local_values_with_der
+from netket.stats import (
+    statistics as _statistics,
+    mean as _mean,
+    sum_inplace as _sum_inplace,
+)
+
+from netket.utils import (
+    MPI_comm as _MPI_comm,
+    n_nodes as _n_nodes,
+    node_number as _rank
+)
+
+import numpy as np
+from scipy.linalg import eig
+
+import netket.optimizer as op
+
+from netket.vmc_common import info, tree_map
+from netket import Vmc
+from netket.abstract_variational_driver import AbstractVariationalDriver
+from threadpoolctl import threadpool_limits
+
+
+class SRStab(Vmc):
+    """
+    Energy minimization using Variational Monte Carlo (VMC).
+    """
+
+    def __init__(
+        self,
+        hamiltonian,
+        sampler,
+        n_samples,
+        sr,
+        diag_shift = 0.01,
+        time_step = 0.02,
+        corr_samp = None,
+        n_discard=None,
+        search_radius=3
+    ):
+        assert(not sr.onthefly)
+        optimizer = op.Sgd(sampler.machine, learning_rate=1.0)
+        super().__init__(hamiltonian, sampler, optimizer, n_samples, n_discard=n_discard, sr=sr)
+        self._diag_shift = diag_shift
+        self._time_step = time_step
+        if corr_samp is None:
+            corr_samples = int(n_samples * 0.35)
+        else:
+            corr_samples = corr_samp
+        n_corr_samples_chain = int(math.ceil((corr_samples / self._batch_size)))
+        self._n_corr_samples_node = int(math.ceil(n_corr_samples_chain / self.n_nodes))
+        self._n_corr_samples = int(self._n_corr_samples_node * self._batch_size * self.n_nodes)
+        self._search_radius = search_radius
+
+    def correlated_en_estimation(self, samples, ref_amplitudes, amplitudes):
+        ratios = np.exp(2 * (amplitudes-ref_amplitudes).real)
+        assert(np.sum(np.isfinite(ratios)) == ratios.size)
+        loc_vals = _local_values(self._ham, self.machine, samples)
+        assert(np.sum(np.isfinite(loc_vals)) == loc_vals.size)
+        return (_mean(loc_vals * ratios)/_mean(ratios))
+
+    def en_estimation(self, samples_r, samples):
+        loc = _local_values(self._ham, self.machine, samples_r).reshape(samples.shape[0:2])
+        assert(np.sum(np.isfinite(loc)) == loc.size)
+        return _statistics(loc)
+
+    def _linesearch(self):
+        samples = None
+        best_e = None
+
+        test_shift = self._diag_shift
+        test_step = self._time_step
+        count = 0
+        valid_result = False
+
+        while not valid_result:
+            try:
+                self._sr._diag_shift = test_shift
+                dp = test_step * self._sr.compute_update(self._jac, self._grads, self._dp)
+                self.machine.parameters -= dp
+                try:
+                    self._sampler.generate_samples(self._n_discard)
+                    samples = self._sampler.generate_samples(self._n_corr_samples_node)
+                    samples_r = samples.reshape((-1, samples.shape[-1]))
+                    ref_amplitudes = self.machine.log_val(samples_r)
+                    stats = self.en_estimation(samples_r, samples)
+                    # TODO: work out good criterion
+                    if ((stats.mean.real) < self._loss_stats.mean.real + 2*self._loss_stats.error_of_mean) and abs(stats.mean.imag) < 1:
+                        valid_result = True
+                    else:
+                        valid_result = False
+                except:
+                    valid_result = False
+                self.machine.parameters += dp
+            except:
+                valid_result = False
+
+            if valid_result:
+                best_e = stats.mean.real
+                best_shift = test_shift
+                best_step = test_step
+                best_dp = dp
+            else:
+                test_shift *= 1.5
+                test_step /= 1.5
+            count += 1
+            assert(count < 100)
+        
+        # randomly sample new parameters from [central/5, 5 * central] (log-uniformly distributed)
+        test_params = []
+        for i in range(4):
+            shift = np.exp(np.random.rand() * (np.log(best_shift*self._search_radius) - np.log(best_shift/self._search_radius)) + np.log(best_shift/self._search_radius))
+            step = np.exp(np.random.rand() * (np.log(best_step*self._search_radius) - np.log(best_step/self._search_radius)) + np.log(best_step/self._search_radius))
+            test_params.append((shift, step))
+
+        for parameters in test_params:
+            self._sr._diag_shift = parameters[0]
+            dp = parameters[1] * self._sr.compute_update(self._jac, self._grads, self._dp)
+            self.machine.parameters -= dp
+            try:
+                amplitudes = self.machine.log_val(samples_r)
+                e_new = self.correlated_en_estimation(samples_r, ref_amplitudes, amplitudes).real
+                valid_result = True
+            except:
+                valid_result = False
+
+            self.machine.parameters += dp
+
+            if valid_result:
+                if e_new < best_e:
+                    best_e = e_new
+                    best_shift = parameters[0]
+                    best_step = parameters[1]
+                    best_dp = dp
+
+        self._diag_shift = best_shift
+        self._time_step = best_step
+        return best_dp
+
+    def _forward_and_backward(self):
+        self._sampler.reset()
+
+        # Burnout phase
+        self._sampler.generate_samples(self._n_discard)
+
+        # Generate samples and store them
+        self._samples = self._sampler.generate_samples(
+            self._n_samples_node, samples=self._samples
+        )
+
+        # Compute the local energy estimator and average Energy
+        eloc, self._loss_stats = self._get_mc_stats(self._ham)
+
+        # Center the local energy
+        eloc -= _mean(eloc)
+
+        samples_r = self._samples.reshape((-1, self._samples.shape[-1]))
+        eloc_r = eloc.reshape(-1, 1)
+
+        # When using the SR (Natural gradient) we need to have the full jacobian
+        self._grads, self._jac = self._machine.vector_jacobian_prod(
+            samples_r,
+            eloc_r / self._n_samples,
+            self._grads,
+            return_jacobian=True,
+        )
+
+        self._grads = tree_map(_sum_inplace, self._grads)
+
+        self._dp = self._linesearch()
+
+        return self._dp
