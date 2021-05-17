35327e646a291cb87ff27883ff66f1030f5e8de7
yannra <yannic.rath@kcl.ac.uk>
2021-05-17 13:45:50 +0100

    Update 1D Heisenberg QGPS script

--- scripts/qgps_1d_heisenberg.py
+++ scripts/qgps_1d_heisenberg.py
@@ -32,7 +32,7 @@ elif mode == 1:
 elif mode == 2:
     ma = nk.machine.QGPSProdSym(hi, n_bond=N, automorphisms=None, spin_flip_sym=False, dtype=complex)
 
-ma.init_random_parameters(sigma=0.05, start_from_uniform=False)
+ma.init_random_parameters(sigma=0.02, start_from_uniform=False)
 
 # Optimizer
 op = nk.optimizer.Sgd(ma, learning_rate=0.02)
@@ -47,7 +47,7 @@ sr = nk.optimizer.SR(ma)
 samples = 4000
 
 # Create the optimization driver
-gs = nk.custom.SweepOpt(hamiltonian=ha, sampler=sa, optimizer=op, n_samples=samples, sr=sr, n_discard=100)
+gs = nk.custom.SweepOpt(hamiltonian=ha, sampler=sa, optimizer=op, n_samples=samples, sr=sr, n_discard=100, max_opt=4000, check_improvement=False, reset_bias=False)
 
 best_epsilon = ma._epsilon.copy()
 best_en_upper_bound = None
@@ -59,27 +59,26 @@ if mpi.COMM_WORLD.Get_rank() == 0:
     np.save("best_epsilon.npy", best_epsilon)
 
 count = 0
-try:
-    for it in gs.iter(3000,1):
-        if mpi.COMM_WORLD.Get_rank() == 0:
-            move("epsilon.npy", "epsilon_old.npy")
-            np.save("epsilon.npy", ma._epsilon)
-            print(it,gs.energy)
-            with open("out.txt", "a") as fl:
-                fl.write("{}  {}  {}\n".format(np.real(gs.energy.mean), np.imag(gs.energy.mean), gs.energy.error_of_mean))
-            if best_en_upper_bound is None:
+for it in gs.iter(2000,1):
+    if mpi.COMM_WORLD.Get_rank() == 0:
+        move("epsilon.npy", "epsilon_old.npy")
+        np.save("epsilon.npy", ma._epsilon)
+        print(it,gs.energy, flush=True)
+        with open("out.txt", "a") as fl:
+            fl.write("{}  {}  {}\n".format(np.real(gs.energy.mean), np.imag(gs.energy.mean), gs.energy.error_of_mean))
+        if best_en_upper_bound is None:
+            best_en_upper_bound = gs.energy.mean.real + gs.energy.error_of_mean
+        else:
+            if (gs.energy.mean.real  + gs.energy.error_of_mean) < best_en_upper_bound:
+                best_epsilon = ma._epsilon.copy()
                 best_en_upper_bound = gs.energy.mean.real + gs.energy.error_of_mean
-            else:
-                if (gs.energy.mean.real  + gs.energy.error_of_mean) < best_en_upper_bound:
-                    best_epsilon = ma._epsilon.copy()
-                    best_en_upper_bound = gs.energy.mean.real + gs.energy.error_of_mean
-                    np.save("best_epsilon.npy", best_epsilon)
-        count += 1
-        if count == 50:
-            count = 0
-            gs.n_samples = gs.n_samples + 100
-except:
-    pass
+                np.save("best_epsilon.npy", best_epsilon)
+        if it == 1949:
+            best_en_upper_bound = None
+    count += 1
+    if count == 50:
+        count = 0
+        gs.n_samples = gs.n_samples + 100
 
 mpi.COMM_WORLD.Bcast(best_epsilon, root=0)
 
@@ -89,7 +88,7 @@ ma._epsilon = best_epsilon
 ma._opt_params = ma._epsilon[ma._der_ids >= 0].copy()
 ma.reset()
 
-est = nk.variational.estimate_expectations(ha, sa, 50000//mpi.COMM_WORLD.size, n_discard=200)
+est = nk.variational.estimate_expectations(ha, sa, 100000//mpi.COMM_WORLD.size, n_discard=200)
 
 if mpi.COMM_WORLD.Get_rank() == 0:
     with open("result.txt", "a") as fl:
