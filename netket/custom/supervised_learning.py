a13b9335e837ab0623e998ffebb1475da218326a
yannra <yannic.rath@kcl.ac.uk>
2021-07-02 12:27:19 +0100

    Clean up supervised opt

--- netket/custom/supervised_learning.py
+++ netket/custom/supervised_learning.py
@@ -84,6 +84,12 @@ class SupervisedLearning():
             return np.sum(np.log(target_amplitudes)*weightings)/np.sum(weightings)
 
 class QGPSLearning(SupervisedLearning):
+    def __init__(self, machine):
+        super().__init__(machine)
+        self.K = None
+        self.weights = None
+        self.alpha_cutoff = 1.e8
+
     @staticmethod
     @njit(parallel=True)
     def kernel_mat_inner(epsilon, ref_site, confs, K, complex_par, Smap, Smap_inverse, sym_spin_flip_sign):
@@ -105,205 +111,169 @@ class QGPSLearning(SupervisedLearning):
                         K[i, 2*x+1] += innerprod
         return K
 
-    def kernel_mat(self, ref_site, confs, K=None):
-        if K is None:
-            K = np.zeros((confs.shape[0], self.machine._epsilon.shape[1] * 2), dtype=self.machine._epsilon.dtype)
+    def set_kernel_mat(self, ref_site, confs):
+        if self.K is None:
+            self.K = np.zeros((confs.shape[0], self.machine._epsilon.shape[1] * 2), dtype=self.machine._epsilon.dtype)
         else:
-            K.fill(0.0)
-        if K.dtype == complex:
+            self.K.fill(0.0)
+        if self.K.dtype == complex:
             complex_par = True
         else:
             complex_par = False
-        return self.kernel_mat_inner(self.machine._epsilon, ref_site, confs, K, complex_par, self.machine._Smap, self.machine._Smap_inverse, self.machine._sym_spin_flip_sign)
+        self.K = self.kernel_mat_inner(self.machine._epsilon, ref_site, confs, self.K, complex_par, self.machine._Smap, self.machine._Smap_inverse, self.machine._sym_spin_flip_sign)
+        return self.K
 
-    def log_marg_lik(self, confset, target_amplitudes, K, noise_tilde=1.e-2, alpha=None):
-        n_bond = self.machine._epsilon.shape[1]
-        n_sites = self.machine._epsilon.shape[0]
-        if alpha is None:
-            alpha_mat = np.zeros(2*n_bond)
-        elif type(alpha) == np.ndarray:
-            alpha_mat = alpha[:].copy()
-        else:
-            alpha_mat = alpha * np.ones(2*n_bond)
-        t = target_amplitudes.astype(self.machine._epsilon.dtype)
-        if noise_tilde == 0.:
-            S_diag = np.ones(len(t))
-        else:
-            S_diag = 1/(np.log1p(noise_tilde/(abs(t)**2)))
-        fit_data = np.log(t)
-        KtK = np.dot(K.conj().T, np.dot(np.diag(S_diag), K))
-        KtK += np.diag(alpha_mat[ :])
-        y = K.conj().T.dot(S_diag * fit_data)
-        log_lik = np.sum(np.log(alpha_mat[:]))
+    def setup_fit_alpha_dep(self, confset, target_amplitudes, ref_site, alpha_mat):
+        self.alpha_mat = alpha_mat
+        self.active_elements = self.alpha_mat < self.alpha_cutoff
+        self.KtK_alpha = self.KtK + np.diag(self.alpha_mat)
         try:
-            L = sp.linalg.cholesky(KtK, lower=True)
-            weights = sp.linalg.cho_solve((L, True), y)
-            Lprime = sp.linalg.solve_triangular(L, np.eye(KtK.shape[0]), check_finite=False, lower=True)
-            log_lik += 2*np.sum(np.log(np.diag(Lprime)))
+            self.L = sp.linalg.cholesky(self.KtK_alpha[np.ix_(self.active_elements, self.active_elements)], lower=True)
+            self.Sinv = sp.linalg.solve_triangular(self.L, np.eye(self.KtK_alpha.shape[0]), check_finite=False, lower=True)
+            weights = sp.linalg.cho_solve((self.L, True), self.y[self.active_elements])
+            self.cholesky = True
         except:
-            L = np.linalg.inv(KtK)
-            weights = L.dot(y)
-            log_lik += np.linalg.slogdet(L)[1]
-        log_lik -= np.sum(np.log(2*np.pi*1/S_diag))
-        log_lik -= np.dot(fit_data.conj(), S_diag * fit_data)
-        log_lik += np.dot(weights.conj(), np.dot(KtK, weights))
+            self.Sinv = np.linalg.inv(self.KtK_alpha[np.ix_(self.active_elements, self.active_elements)])
+            weights = self.Sinv.dot(self.y[self.active_elements])
+            self.cholesky = False
+
+        if self.weights is None:
+            self.weights = np.zeros(self.alpha_mat.shape[0], dtype=weights.dtype)
+        self.weights[self.active_elements] = weights
+
+    def setup_fit_noise_dep(self, confset, target_amplitudes, ref_site, noise_tilde, alpha_mat):
+        self.noise_tilde = noise_tilde
+        if noise_tilde == 0.:
+            self.S_diag = np.ones(len(self.log_amps))
+        else:
+            self.S_diag = 1/(np.log1p(self.noise_tilde/(abs(self.log_amps)**2)))
+
+        # parallelise this
+        self.KtK = np.dot(self.K.conj().T, np.dot(np.diag(self.S_diag), self.K))
+        self.y = self.K.conj().T.dot(self.S_diag * self.fit_data)
+
+        self.setup_fit_alpha_dep(confset, target_amplitudes, ref_site, alpha_mat)
+
+    def setup_fit(self, confset, target_amplitudes, ref_site, noise_tilde, alpha_mat):
+        self.log_amps = target_amplitudes.astype(self.machine._epsilon.dtype)
+        self.fit_data = np.log(self.log_amps)
+        self.set_kernel_mat(ref_site, confset)
+        self.setup_fit_noise_dep(confset, target_amplitudes, ref_site, noise_tilde, alpha_mat)
+
+    def log_marg_lik(self):
+        # parallelise
+        log_lik = -(np.sum(np.log(2*np.pi*1/self.S_diag)))
+        if self.cholesky:
+            log_lik += 2*np.sum(np.log(np.diag(self.Sinv)))
+        else:
+            log_lik += np.linalg.slogdet(self.Sinv)[1]
+        log_lik -= np.dot(self.fit_data.conj(), self.S_diag * self.fit_data)
+        #
+
+        log_lik += np.sum(np.log(self.alpha_mat[self.active_elements]))
+        weights = self.weights[self.active_elements]
+        log_lik += np.dot(weights.conj(), np.dot(self.KtK_alpha[np.ix_(self.active_elements, self.active_elements)], weights))
         return 0.5*log_lik.real
 
-    def log_marg_lik_noise_der(self, confset, target_amplitudes, K, noise_tilde=1.e-2, alpha=None):
-        n_bond = self.machine._epsilon.shape[1]
-        n_sites = self.machine._epsilon.shape[0]
-        if alpha is None:
-            alpha_mat = np.zeros(2*n_bond)
-        elif type(alpha) == np.ndarray:
-            alpha_mat = alpha[:].copy()
+    def log_marg_lik_noise_der(self):
+        del_S = 1/((abs(self.log_amps)**2) * (1 + self.noise_tilde/(abs(self.log_amps)**2)))
+        Delta_S = - (self.S_diag**2 * del_S)
+
+        # parallelise
+        derivative_noise = -np.sum(self.S_diag * del_S)
+
+        K = self.K[:,self.active_elements]
+        if self.cholesky:
+            derivative_noise -= np.trace(K.conj().T.dot(np.diag(Delta_S).dot(K.dot(self.Sinv.T.dot(self.Sinv)))))
         else:
-            alpha_mat = alpha * np.ones(2*n_bond)
-        t = target_amplitudes.astype(self.machine._epsilon.dtype)
-        if noise_tilde == 0.:
-            S_diag = np.ones(len(t))
-        else:
-            S_diag = 1/(np.log1p(noise_tilde/(abs(t)**2)))
-        fit_data = np.log(t)
-        KtK = np.dot(K.conj().T, np.dot(np.diag(S_diag), K))
-        KtK += np.diag(alpha_mat[ :])
-        y = K.conj().T.dot(S_diag * fit_data)
-        del_S = 1/((abs(t)**2) * (1 + noise_tilde/(abs(t)**2)))
-        derivative_noise = -np.sum(S_diag * del_S)
-        Delta_S = - (S_diag**2 * del_S)
-        try:
-            L = sp.linalg.cholesky(KtK, lower=True)
-            weights = sp.linalg.cho_solve((L, True), y)
-            Sinv = sp.linalg.solve_triangular(L, np.eye(KtK.shape[0]), check_finite=False, lower=True)
-            derivative_noise -= np.trace(K.conj().T.dot(np.diag(Delta_S).dot(K.dot(Sinv.T.dot(Sinv)))))
-        except:
-            print("Warning, cholesky failed!")
-            Sinv = np.linalg.inv(KtK)
-            weights = Sinv.dot(y)
-            derivative_noise -= np.trace(K.conj().T.dot(np.diag(Delta_S).dot(K.dot(Sinv))))
-        derivative_noise -= fit_data.conj().dot(Delta_S*fit_data)
+            derivative_noise -= np.trace(K.conj().T.dot(np.diag(Delta_S).dot(K.dot(self.Sinv))))
+
+        weights = self.weights[self.active_elements]
+
+        derivative_noise -= self.fit_data.conj().dot(Delta_S*self.fit_data)
         derivative_noise -= weights.conj().dot(K.conj().T.dot(Delta_S*K.dot(weights)))
-        derivative_noise += 2*fit_data.conj().dot(Delta_S*K.dot(weights))
+        derivative_noise += 2*self.fit_data.conj().dot(Delta_S*K.dot(weights))
+
         return 0.5*derivative_noise.real
     
-    def log_marg_lik_alpha_der(self, confset, target_amplitudes, K, noise_tilde=1.e-2, alpha=None):
-        n_bond = self.machine._epsilon.shape[1]
-        n_sites = self.machine._epsilon.shape[0]
-        if alpha is None:
-            alpha_mat = np.zeros(2*n_bond)
-        elif type(alpha) == np.ndarray:
-            alpha_mat = alpha[:].copy()
+    def log_marg_lik_alpha_der(self):
+        derivative_alpha = 1/self.alpha_mat
+        if self.cholesky:
+            derivative_alpha[self.active_elements] -= np.sum(abs(self.Sinv) ** 2, 0)
         else:
-            alpha_mat = alpha * np.ones(2*n_bond)
-        t = target_amplitudes.astype(self.machine._epsilon.dtype)
-        if noise_tilde == 0.:
-            S_diag = np.ones(len(t))
-        else:
-            S_diag = 1/(np.log1p(noise_tilde/(abs(t)**2)))
-        fit_data = np.log(t)
-        KtK = np.dot(K.conj().T, np.dot(np.diag(S_diag), K))
-        KtK += np.diag(alpha_mat[ :])
-        y = K.conj().T.dot(S_diag * fit_data)
-        derivative_alpha = 1/alpha_mat
-        try:
-            L = sp.linalg.cholesky(KtK, lower=True)
-            weights = sp.linalg.cho_solve((L, True), y)
-            Sinv = sp.linalg.solve_triangular(L, np.eye(KtK.shape[0]), check_finite=False, lower=True)
-            derivative_alpha -= np.sum(abs(Sinv) ** 2, 0)
-        except:
-            print("Warning, cholesky failed!")
-            Sinv = np.linalg.inv(KtK)
-            weights = Sinv.dot(y)
-            derivative_alpha -= np.diag(Sinv).real
-        derivative_alpha -= (weights.conj() * weights).real
+            derivative_alpha[self.active_elements] -= np.diag(self.Sinv).real
+
+        derivative_alpha -= (self.weights.conj() * self.weights).real
+
         return 0.5*derivative_alpha.real
 
     def fit_step(self, confset, target_amplitudes, ref_site, noise_tilde, alpha, noise_range=(1.e-10, 1.e5), alpha_range=(1.e-10, 1.e7),
-                 opt_alpha=True, opt_noise=True, max_iterations=20, rvm=False):
-        n_bond = self.machine._epsilon.shape[1]
-        K = self.kernel_mat(ref_site, confset)
-        alpha_mat = alpha[ref_site, :].copy()
+                 opt_alpha=True, opt_noise=True, max_iterations=10, rvm=False):
+        self.setup_fit(confset, target_amplitudes, ref_site, noise_tilde[ref_site], alpha[ref_site, :].copy())
         if opt_noise:
-            def derivative(x):
-                der_noise = self.log_marg_lik_noise_der(confset, target_amplitudes, K, noise_tilde=np.exp(x[0]), alpha=np.exp(x[1]))
+            def ML(x):
                 if opt_alpha:
                     if rvm:
-                        der_alpha = self.log_marg_lik_alpha_der(confset, target_amplitudes, K, noise_tilde=np.exp(x[0]), alpha=np.exp(x[1:]))
+                        np.copyto(self.alpha_mat, np.exp(x[1:]))
+                    else:
+                        self.alpha_mat.fill(np.exp(x[1]))
+                self.setup_fit_noise_dep(confset, target_amplitudes, ref_site, np.exp(x[0]), self.alpha_mat)
+                return -self.log_marg_lik()
+
+            def derivative(x):
+                if opt_alpha:
+                    if rvm:
+                        np.copyto(self.alpha_mat, np.exp(x[1:]))
+                    else:
+                        self.alpha_mat.fill(np.exp(x[1]))
+                self.setup_fit_noise_dep(confset, target_amplitudes, ref_site, np.exp(x[0]), self.alpha_mat)
+                der_noise = self.log_marg_lik_noise_der()
+                if opt_alpha:
+                    der_alpha = self.log_marg_lik_alpha_der()
+                    if rvm:
                         return np.concatenate((np.array([-der_noise]), -der_alpha))*np.exp(x)
                     else:
-                        der_alpha = self.log_marg_lik_alpha_der(confset, target_amplitudes, K, noise_tilde=np.exp(x[0]), alpha=np.exp(x[1]))
                         return np.array([-der_noise, -np.sum(der_alpha)])*np.exp(x)
                 else:
                     return - der_noise * np.exp(x)
+            
+            bounds = [(np.log(noise_range[0]), np.log(noise_range[1]))]
+            init = [np.log(noise_tilde[ref_site])]
+
+            if opt_alpha:
+                bounds.append((np.log(alpha_range[0]), np.log(alpha_range[1])))
+                init.append(np.log(alpha[ref_site, 0]))
+                if rvm:
+                    for i in range(1, alpha.shape[1]):
+                        bounds.append((np.log(alpha_range[0]), np.log(alpha_range[1])))
+                        init.append(np.log(alpha[ref_site, i]))
+
+            opt = sp.optimize.minimize(ML, np.array(init), options={"maxiter" : max_iterations}, jac=derivative, bounds=bounds)
+            new_hyperpar = np.exp(opt.x)
+            
+            noise_tilde[ref_site] = new_hyperpar[0]
+
             if opt_alpha:
                 if rvm:
-                    bounds = [(np.log(noise_range[0]), np.log(noise_range[1]))]
-                    for i in range(alpha.shape[1]):
-                        bounds.append((np.log(alpha_range[0]), np.log(alpha_range[1])))
-
-                    ML = lambda x : -self.log_marg_lik(confset, target_amplitudes, K, noise_tilde=np.exp(x[0]), alpha=np.exp(x[1:]))
-                    opt = sp.optimize.minimize(ML, np.concatenate((np.array([np.log(noise_tilde[ref_site])]), np.log(alpha_mat))), options={"maxiter" : max_iterations}, jac=derivative, bounds=bounds)
-                    new_hyperpar = np.exp(opt.x)
-                    noise_tilde[ref_site] = new_hyperpar[0]
-                    np.copyto(alpha_mat, new_hyperpar[1:])
+                    np.copyto(self.alpha_mat, new_hyperpar[1:])
                 else:
-                    ML = lambda x : -self.log_marg_lik(confset, target_amplitudes, K, noise_tilde=np.exp(x[0]), alpha=np.exp(x[1]))
-                    opt = sp.optimize.minimize(ML, np.array([np.log(noise_tilde[ref_site]), np.log(alpha_mat[0])]), options={"maxiter" : max_iterations}, jac=derivative, bounds=[(np.log(noise_range[0]), np.log(noise_range[1])), (np.log(alpha_range[0]), np.log(alpha_range[1]))])
-                    new_hyperpar = np.exp(opt.x)
-                    noise_tilde[ref_site] = new_hyperpar[0]
-                    alpha_mat[:] = new_hyperpar[1]
-            else:
-                ML = lambda x : -self.log_marg_lik(confset, target_amplitudes, K, noise_tilde=np.exp(x[0]), alpha=alpha_mat)
-                opt = sp.optimize.minimize(ML, np.log(noise_tilde[ref_site]), options={"maxiter" : max_iterations}, jac=derivative, bounds=[(np.log(noise_range[0]), np.log(noise_range[1]))])
-                new_hyperpar = np.exp(opt.x)
-                noise_tilde[ref_site] = new_hyperpar
+                    self.alpha_mat.fill(new_hyperpar[1])
+
+            self.setup_fit_noise_dep(confset, target_amplitudes, ref_site, noise_tilde[ref_site], self.alpha_mat)
+
         elif opt_alpha:
-            noise = noise_tilde[ref_site]
-            t = target_amplitudes.astype(self.machine._epsilon.dtype)
-            if noise == 0.:
-                S_diag = np.ones(len(t))
-            else:
-                S_diag = 1/(np.log1p(noise/(abs(t)**2)))
-            fit_data = np.log(t)
-            KtK = np.dot(K.conj().T, np.dot(np.diag(S_diag), K))
             for i in range(max_iterations):
-                active_elements = alpha_mat < alpha_range[1]
-                y = (K.conj().T.dot(S_diag * fit_data))[active_elements]
-                KtK_alph = (KtK + np.diag(alpha_mat[ :]))[np.ix_(active_elements, active_elements)]
-                try:
-                    L = sp.linalg.cholesky(KtK_alph, lower=True)
-                    weights = sp.linalg.cho_solve((L, True), y)
-                    Sinv = sp.linalg.solve_triangular(L, np.eye(KtK_alph.shape[0]), check_finite=False, lower=True)
-                    gamma = (1 - (alpha_mat[active_elements])*np.sum(abs(Sinv) ** 2, 0))
-                except:
-                    print("Warning, cholesky failed!")
-                    Sinv = np.linalg.inv(KtK_alph)
-                    weights = Sinv.dot(y)
-                    gamma = (1 - (alpha_mat[active_elements])*np.diag(Sinv).real)
-                if rvm:
-                    alpha_mat[active_elements] = (gamma/(weights.conj()*weights)).real
+                if self.cholesky:
+                    gamma = (1 - (self.alpha_mat[self.active_elements])*np.sum(abs(self.Sinv) ** 2, 0))
                 else:
-                    alpha_mat.fill((np.sum(gamma)/(weights.conj().dot(weights))).real)
+                    gamma = (1 - (self.alpha_mat[self.active_elements])*np.diag(self.Sinv).real)
+                if rvm:
+                    self.alpha_mat[self.active_elements] = (gamma/((self.weights.conj()*self.weights)[self.active_elements])).real
+                else:
+                    self.alpha_mat.fill((np.sum(gamma)/(self.weights.conj().dot(self.weights))).real)
 
-        np.copyto(alpha[ref_site,:], alpha_mat)
+                self.setup_fit_alpha_dep(confset, target_amplitudes, ref_site, self.alpha_mat)
 
-        noise = noise_tilde[ref_site]
-        t = target_amplitudes.astype(self.machine._epsilon.dtype)
-        if noise == 0.:
-            S_diag = np.ones(len(t))
-        else:
-            S_diag = 1/(np.log1p(noise/(abs(t)**2)))
-        fit_data = np.log(t)
-        KtK = np.dot(K.conj().T, np.dot(np.diag(S_diag), K))
-        active_elements = alpha_mat < alpha_range[1]
-        KtK_alph = (KtK + np.diag(alpha_mat[ :]))[np.ix_(active_elements, active_elements)]
-        y = (K.conj().T.dot(S_diag * fit_data))[active_elements]
-        try:
-            L = sp.linalg.cholesky(KtK_alph, lower=True)
-            weights = sp.linalg.cho_solve((L, True), y)
-        except:
-            print("Warning, cholesky failed2!")
-            weights = np.linalg.lstsq(KtK_alph, y)[0]
-        full_weights = np.zeros(KtK.shape[0], dtype=weights.dtype)
-        full_weights[active_elements] = weights
-        self.machine._epsilon[ref_site, :, :] = full_weights.reshape(n_bond, 2)
+        alpha[ref_site, :] = self.alpha_mat
+        self.machine._epsilon[ref_site, :, :] = self.weights.reshape(self.machine._epsilon.shape[1], 2)
         return
