703f8263054ed937aeda2b2e4aa03ef133fee325
yannra <yannic.rath@kcl.ac.uk>
2021-07-05 14:11:11 +0100

    Clean up supervised learning

--- netket/custom/supervised_learning.py
+++ netket/custom/supervised_learning.py
@@ -96,11 +96,14 @@ class SupervisedLearning():
             return _MPI_comm.allreduce(np.sum(np.log(target_amplitudes)*weightings))/_MPI_comm.allreduce(np.sum(weightings))
 
 class QGPSLearning(SupervisedLearning):
-    def __init__(self, machine):
+    def __init__(self, machine, init_alpha = 1.0, init_noise_tilde = 1.e-1):
         super().__init__(machine)
         self.K = None
         self.weights = None
+        self.noise_tilde = init_noise_tilde
+        self.alpha_mat = np.ones((self.machine._epsilon.shape[0], 2*self.machine._epsilon.shape[1]))*init_alpha
         self.alpha_cutoff = 1.e8
+        assert(init_alpha < self.alpha_cutoff)
 
     @staticmethod
     @njit()
@@ -123,7 +126,7 @@ class QGPSLearning(SupervisedLearning):
                         K[i, 2*x+1] += innerprod
         return K
 
-    def set_kernel_mat(self, ref_site, confs):
+    def set_kernel_mat(self, confs):
         if self.K is None:
             self.K = np.zeros((confs.shape[0], self.machine._epsilon.shape[1] * 2), dtype=self.machine._epsilon.dtype)
         else:
@@ -132,13 +135,12 @@ class QGPSLearning(SupervisedLearning):
             complex_par = True
         else:
             complex_par = False
-        self.K = self.kernel_mat_inner(self.machine._epsilon, ref_site, confs, self.K, complex_par, self.machine._Smap, self.machine._Smap_inverse, self.machine._sym_spin_flip_sign)
+        self.K = self.kernel_mat_inner(self.machine._epsilon, self.ref_site, confs, self.K, complex_par, self.machine._Smap, self.machine._Smap_inverse, self.machine._sym_spin_flip_sign)
         return self.K
 
-    def setup_fit_alpha_dep(self, confset, target_amplitudes, ref_site, alpha_mat):
-        self.alpha_mat = alpha_mat
-        self.active_elements = self.alpha_mat < self.alpha_cutoff
-        self.KtK_alpha = self.KtK + np.diag(self.alpha_mat)
+    def setup_fit_alpha_dep(self, confset, target_amplitudes):
+        self.active_elements = self.alpha_mat[self.ref_site,:] < self.alpha_cutoff
+        self.KtK_alpha = self.KtK + np.diag(self.alpha_mat[self.ref_site,:])
         
         try:
             L = sp.linalg.cholesky(self.KtK_alpha[np.ix_(self.active_elements, self.active_elements)], lower=True)
@@ -153,14 +155,13 @@ class QGPSLearning(SupervisedLearning):
             #     print("Warning! Cholesky failed.")
 
         if self.weights is None:
-            self.weights = np.zeros(self.alpha_mat.shape[0], dtype=weights.dtype)
+            self.weights = np.zeros(self.alpha_mat.shape[1], dtype=weights.dtype)
         
         # potentially distribute weights across processes
         self.weights[self.active_elements] = weights
 
-    def setup_fit_noise_dep(self, confset, target_amplitudes, ref_site, noise_tilde, alpha_mat):
-        self.noise_tilde = noise_tilde
-        if noise_tilde == 0.:
+    def setup_fit_noise_dep(self, confset, target_amplitudes):
+        if self.noise_tilde == 0.:
             self.S_diag = np.ones(len(self.log_amps))
         else:
             self.S_diag = 1/(np.log1p(self.noise_tilde/(abs(self.log_amps)**2)))
@@ -168,13 +169,14 @@ class QGPSLearning(SupervisedLearning):
         self.KtK = _sum_inplace(np.dot(self.K.conj().T, np.dot(np.diag(self.S_diag), self.K)))
         self.y = _sum_inplace(self.K.conj().T.dot(self.S_diag * self.fit_data))
 
-        self.setup_fit_alpha_dep(confset, target_amplitudes, ref_site, alpha_mat)
+        self.setup_fit_alpha_dep(confset, target_amplitudes)
 
-    def setup_fit(self, confset, target_amplitudes, ref_site, noise_tilde, alpha_mat):
+    def setup_fit(self, confset, target_amplitudes, ref_site):
+        self.ref_site = ref_site
         self.log_amps = target_amplitudes.astype(self.machine._epsilon.dtype)
         self.fit_data = np.log(self.log_amps)
-        self.set_kernel_mat(ref_site, confset)
-        self.setup_fit_noise_dep(confset, target_amplitudes, ref_site, noise_tilde, alpha_mat)
+        self.set_kernel_mat(confset)
+        self.setup_fit_noise_dep(confset, target_amplitudes)
 
     def log_marg_lik(self):
         log_lik = -(np.sum(np.log(2*np.pi*1/self.S_diag)))
@@ -186,7 +188,7 @@ class QGPSLearning(SupervisedLearning):
         else:
             log_lik += np.linalg.slogdet(self.Sinv)[1]
 
-        log_lik += np.sum(np.log(self.alpha_mat[self.active_elements]))
+        log_lik += np.sum(np.log(self.alpha_mat[self.ref_site, :]))
         weights = self.weights[self.active_elements]
         log_lik += np.dot(weights.conj(), np.dot(self.KtK_alpha[np.ix_(self.active_elements, self.active_elements)], weights))
         return 0.5*log_lik.real
@@ -214,7 +216,7 @@ class QGPSLearning(SupervisedLearning):
         return 0.5*derivative_noise.real
     
     def log_marg_lik_alpha_der(self):
-        derivative_alpha = 1/self.alpha_mat
+        derivative_alpha = 1/(self.alpha_mat[self.ref_site, :])
         if self.cholesky:
             derivative_alpha[self.active_elements] -= np.sum(abs(self.Sinv) ** 2, 0)
         else:
@@ -224,26 +226,28 @@ class QGPSLearning(SupervisedLearning):
 
         return 0.5*derivative_alpha.real
 
-    def fit_step(self, confset, target_amplitudes, ref_site, noise_tilde, alpha, noise_range=(1.e-10, 1.e5), alpha_range=(1.e-10, 1.e7),
+    def fit_step(self, confset, target_amplitudes, ref_site, noise_range=(1.e-10, 1.e5), alpha_range=(1.e-10, 1.e7),
                  opt_alpha=True, opt_noise=True, max_iterations=10, rvm=False):
-        self.setup_fit(confset, target_amplitudes, ref_site, noise_tilde[ref_site], alpha[ref_site, :].copy())
+        self.setup_fit(confset, target_amplitudes, ref_site)
         if opt_noise:
             def ML(x):
                 if opt_alpha:
                     if rvm:
-                        np.copyto(self.alpha_mat, np.exp(x[1:]))
+                        self.alpha_mat[self.ref_site, :] = np.exp(x[1:])
                     else:
-                        self.alpha_mat.fill(np.exp(x[1]))
-                self.setup_fit_noise_dep(confset, target_amplitudes, ref_site, np.exp(x[0]), self.alpha_mat)
+                        self.alpha_mat[self.ref_site, :] = np.exp(x[1])
+                self.noise_tilde = np.exp(x[0])
+                self.setup_fit_noise_dep(confset, target_amplitudes)
                 return -self.log_marg_lik()
 
             def derivative(x):
                 if opt_alpha:
                     if rvm:
-                        np.copyto(self.alpha_mat, np.exp(x[1:]))
+                        self.alpha_mat[self.ref_site, :] = np.exp(x[1:])
                     else:
-                        self.alpha_mat.fill(np.exp(x[1]))
-                self.setup_fit_noise_dep(confset, target_amplitudes, ref_site, np.exp(x[0]), self.alpha_mat)
+                        self.alpha_mat[self.ref_site, :] = np.exp(x[1])
+                self.noise_tilde = np.exp(x[0])
+                self.setup_fit_noise_dep(confset, target_amplitudes)
                 der_noise = self.log_marg_lik_noise_der()
                 if opt_alpha:
                     der_alpha = self.log_marg_lik_alpha_der()
@@ -255,42 +259,41 @@ class QGPSLearning(SupervisedLearning):
                     return - der_noise * np.exp(x)
             
             bounds = [(np.log(noise_range[0]), np.log(noise_range[1]))]
-            init = [np.log(noise_tilde[ref_site])]
+            init = [np.log(self.noise_tilde)]
 
             if opt_alpha:
                 bounds.append((np.log(alpha_range[0]), np.log(alpha_range[1])))
-                init.append(np.log(alpha[ref_site, 0]))
+                init.append(np.log(self.alpha_mat[self.ref_site, 0]))
                 if rvm:
-                    for i in range(1, alpha.shape[1]):
+                    for i in range(1, self.alpha_mat.shape[1]):
                         bounds.append((np.log(alpha_range[0]), np.log(alpha_range[1])))
-                        init.append(np.log(alpha[ref_site, i]))
+                        init.append(np.log(self.alpha_mat[self.ref_site, i]))
 
             opt = sp.optimize.minimize(ML, np.array(init), options={"maxiter" : max_iterations}, jac=derivative, bounds=bounds)
             new_hyperpar = np.exp(opt.x)
             
-            noise_tilde[ref_site] = new_hyperpar[0]
+            self.noise_tilde = new_hyperpar[0]
 
             if opt_alpha:
                 if rvm:
-                    np.copyto(self.alpha_mat, new_hyperpar[1:])
+                    self.alpha_mat[self.ref_site, :] = new_hyperpar[1:]
                 else:
-                    self.alpha_mat.fill(new_hyperpar[1])
+                    self.alpha_mat[self.ref_site, :] = new_hyperpar[1]
 
-            self.setup_fit_noise_dep(confset, target_amplitudes, ref_site, noise_tilde[ref_site], self.alpha_mat)
+            self.setup_fit_noise_dep(confset, target_amplitudes)
 
         elif opt_alpha:
             for i in range(max_iterations):
                 if self.cholesky:
-                    gamma = (1 - (self.alpha_mat[self.active_elements])*np.sum(abs(self.Sinv) ** 2, 0))
+                    gamma = (1 - (self.alpha_mat[self.ref_site, self.active_elements])*np.sum(abs(self.Sinv) ** 2, 0))
                 else:
-                    gamma = (1 - (self.alpha_mat[self.active_elements])*np.diag(self.Sinv).real)
+                    gamma = (1 - (self.alpha_mat[self.ref_site, self.active_elements])*np.diag(self.Sinv).real)
                 if rvm:
-                    self.alpha_mat[self.active_elements] = (gamma/((self.weights.conj()*self.weights)[self.active_elements])).real
+                    self.alpha_mat[self.ref_site, self.active_elements] = (gamma/((self.weights.conj()*self.weights)[self.active_elements])).real
                 else:
-                    self.alpha_mat.fill((np.sum(gamma)/(self.weights.conj().dot(self.weights))).real)
+                    self.alpha_mat[self.ref_site, :] = ((np.sum(gamma)/(self.weights.conj().dot(self.weights))).real)
 
-                self.setup_fit_alpha_dep(confset, target_amplitudes, ref_site, self.alpha_mat)
+                self.setup_fit_alpha_dep(confset, target_amplitudes)
 
-        alpha[ref_site, :] = self.alpha_mat
         self.machine._epsilon[ref_site, :, :] = self.weights.reshape(self.machine._epsilon.shape[1], 2)
         return
